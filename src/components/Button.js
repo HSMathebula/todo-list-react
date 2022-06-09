99088140686a8ba2e5ef2e493b69daff2851d08d
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-09 20:11:55 +0200

    add styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,15 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text, onClick }) => {
-    return (
-      <button
-        onClick={onClick}
-        style={{ backgroundColor: color }}
-        className='btn'
-      >
-        {text}
-      </button>
-    )
-  }
+const Button = ({ color, text, onClick }) => (
+  <button
+    type="submit"
+    onClick={onClick}
+    style={{ backgroundColor: color }}
+    className="btn"
+  >
+    {text}
+  </button>
+);
 
 Button.defaultProps = {
   color: 'steelblue',
